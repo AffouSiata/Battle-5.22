942db6370e99873042ac2233bcdf0ca2c4c9767b
AffouSiata <affousiatao59@gmail.com>
2022-09-03 16:32:27 +0000

    fhjfhjfhj

---
+++ fonction1/app.js
@@ -0,0 +1,27 @@
+function triangle(nombre1,nombre2){
+    let n = Math.sqrt(nombre1);
+    let nn = Math.sqrt(nombre2);
+    let res = n+nn
+    
+
+}
+triangle()
+
+
+
+
+
+
+// let nombre1 =16;
+
+// let n = Math.sqrt(nombre1.pow());
+// let nombre2 =2;
+
+// let n = Math.sqrt(nombre1);
+// let nn = Math.sqrt(nombre2);
+// let res = n+nn
+// console.log(n);
+    
+
+
+ 
