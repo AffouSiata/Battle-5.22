4b017d09ba53dd8d95fc3dafd4949d7ab5bd6d35
AffouSiata <affousiatao59@gmail.com>
2022-09-03 15:46:31 +0000

    first

---
+++ Battle/page2.js
@@ -0,0 +1,13 @@
+
+function alphabet(string){
+
+    let  alpha = "";
+    // console.log(renverser);
+   for (let index = 0; index < array.length; index++) {
+       const element = array[index];
+       
+   }
+
+}
+
+alphabet("azerty")
