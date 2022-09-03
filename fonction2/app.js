1691f1b64653af250bb9a886cabf2613ae1a68ae
AffouSiata <affousiatao59@gmail.com>
2022-09-03 16:02:56 +0000

    jdfhdjhf

---
+++ fonction2/app.js
@@ -0,0 +1,18 @@
+function nombre(int){
+
+    let nombre1 = parseInt(prompt("entrer un nombre"))
+        let racine = Math.sqrt(nombre1);
+
+        console.log(racine);
+
+        let nombre2 = 0;
+        for (let i = 2; i < ; i++) {
+            const element = array[i];
+            
+        }
+
+}
+nombre()
+
+
+
